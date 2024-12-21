e55b9be991c8de14240dbffef0f709ed6da01f47
EmotionlessDev <emotionlesscode@gmail.com>
2024-12-21 20:56:19 +1000

    added comments layout

---
+++ frontend/src/components/ReplyCommentForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form id="reply-form" class="d-none mt-4">
+    <fieldset>
+      <div class="mb-3">
+        <label for="reply-comment" class="form-label">Your Reply</label>
+        <textarea
+          id="reply-comment"
+          class="form-control"
+          rows="3"
+          placeholder="Write a reply..."
+        ></textarea>
+      </div>
+      <button
+        type="submit"
+        class="mb-2 btn border-start-0 border-top-0 btn-outline-primary mt-2 action-unchecked comments__reply"
+      >
+        Submit Reply
+      </button>
+    </fieldset>
+  </form>
+</template>
+
+<style>
+
+</style>
+
+<script setup lang="ts">
+
+</script>
