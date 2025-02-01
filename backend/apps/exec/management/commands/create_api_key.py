b0efa2df21fce81c7e9dfde71683e88c399b5b14
teenxsky <kutorgin2002@gmail.com>
2025-02-01 17:54:09 +1000

    add warning message when api key already exists

--- backend/apps/exec/management/commands/create_api_key.py
+++ backend/apps/exec/management/commands/create_api_key.py
@@ -1,10 +1,15 @@
 from rest_framework_api_key.models import APIKey
 from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseCommand):
     help = "Create a new API key"
 
     def handle(self, *args, **kwargs):
-        api_key, key = APIKey.objects.create_key(name="TYF_API_KEY")
-        self.stdout.write(self.style.SUCCESS(f"API Key created: {key}"))
+        try:
+            _ = APIKey.objects.get(name="TYF_API_KEY")
+            self.stdout.write(self.style.WARNING("API Key already exists."))
+        except ObjectDoesNotExist:
+            _, key = APIKey.objects.create_key(name="TYF_API_KEY")
+            self.stdout.write(self.style.SUCCESS(f"API Key created: {key}"))
